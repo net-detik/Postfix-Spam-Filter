1a8e159df16efeb6cab8f3f5d6db27d501d8236a
zakimanhamid <zakimanhamid@gmail.com>
2019-08-16 09:38:39 +0700

    Update run.spam.algoritma.php

--- run.spam.algoritma.php
+++ run.spam.algoritma.php
@@ -3,7 +3,9 @@ define("PLATFORM_ROOT", __DIR__ ."/");
 error_reporting('none');
 require_once(PLATFORM_ROOT.'spam.algoritma.php');
 
+$config['ToleransiMax']=15;
+
 $SA=new spamAlgoritma();
-$mailq=$SA::findSpam();
+$mailq=$SA::findSpam($config);
 
 #echo '<pre>'.print_r($mailq,true).'</pre>';
