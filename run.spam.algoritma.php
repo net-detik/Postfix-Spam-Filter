6ca603d59848ee8f706abebdcf32d2d0a77f62a1
zakimanhamid <zakimanhamid@gmail.com>
2019-08-16 09:40:34 +0700

    Update run.spam.algoritma.php

--- run.spam.algoritma.php
+++ run.spam.algoritma.php
@@ -3,6 +3,7 @@ define("PLATFORM_ROOT", __DIR__ ."/");
 error_reporting('none');
 require_once(PLATFORM_ROOT.'spam.algoritma.php');
 
+//maksimal jumlah toleransi trafik email dalam waktu yang sama
 $config['ToleransiMax']=15;
 
 $SA=new spamAlgoritma();
